8ecac8c676720e548df7a6858a24466363f8418c
Fernando Bevilacqua <dovyski@gmail.com>
2018-01-31 08:16:43 +0100

    Level type is a config param now

--- cots-mario/experiment/setup.js
+++ cots-mario/experiment/setup.js
@@ -4,26 +4,26 @@
  */
 
 var EXPERIMENT_GAME_PROFILES = {
-	'a': {levelName: 'A1', sessionMaxDurationSeconds: 0, levelWidth: 1000, seed: 200, marioLives: 3, marioLarge: true, levelDifficulty: 0, levelFlowerAllowed: false, ValueMinOddsTubes: 0, ValueMultiOddsTubes: 0, ValueMinOddsJump: 0, ValueMultiOddsJump: 0, CoinsMinLineStartOffset: 10, CoinsMinLineEndOffset: 10, BlocksMinLineStartOffset: 10, BlocksMinLineEndOffset: 10, ValueOddsStraight: 10, ValueOddsHillStraight: 0, EnemyCreationCeilControl: 400},
-	'b': {levelName: 'B2', sessionMaxDurationSeconds: 0, levelWidth: 800, seed: 201, marioLives: 3, marioLarge: true, levelDifficulty: 0, levelFlowerAllowed: false, ValueMinOddsTubes: 0, ValueMultiOddsTubes: 0, ValueMinOddsJump: 0, ValueMultiOddsJump: 0, CoinsMinLineStartOffset: 7, CoinsMinLineEndOffset: 7, BlocksMinLineStartOffset: 7, BlocksMinLineEndOffset: 7, ValueOddsStraight: 10, ValueOddsHillStraight: 0, EnemyCreationCeilControl: 150},
-	'c': {levelName: 'C3', sessionMaxDurationSeconds: 0, levelWidth: 800, seed: 202, marioLives: 3, marioLarge: true, levelDifficulty: 0, levelFlowerAllowed: false, ValueMinOddsTubes: 0, ValueMultiOddsTubes: 0, ValueMinOddsJump: 0, ValueMultiOddsJump: 0, CoinsMinLineStartOffset: 3, CoinsMinLineEndOffset: 3, BlocksMinLineStartOffset: 5, BlocksMinLineEndOffset: 5, ValueOddsStraight: 10, ValueOddsHillStraight: 3, EnemyCreationCeilControl: 125},
-	'd': {levelName: 'D4', sessionMaxDurationSeconds: 0, levelWidth: 800, seed: 203, marioLives: 3, marioLarge: false, levelDifficulty: 1, levelFlowerAllowed: false, ValueMinOddsTubes: 0, ValueMultiOddsTubes: 0, ValueMinOddsJump: 1, ValueMultiOddsJump: 1, CoinsMinLineStartOffset: 1, CoinsMinLineEndOffset: 1, BlocksMinLineStartOffset: 2, BlocksMinLineEndOffset: 2, ValueOddsStraight: 20, ValueOddsHillStraight: 10, EnemyCreationCeilControl: 100},
-	'e': {levelName: 'E5', sessionMaxDurationSeconds: 0, levelWidth: 800, seed: 204, marioLives: 3, marioLarge: false, levelDifficulty: 1},
-	'f': {levelName: 'F6', sessionMaxDurationSeconds: 0, levelWidth: 700, seed: 205, marioLives: 3, marioLarge: false, levelDifficulty: 2},
-	'g': {levelName: 'G7', sessionMaxDurationSeconds: 0, levelWidth: 700, seed: 206, marioLives: 3, marioLarge: false, levelDifficulty: 2},
-	'h': {levelName: 'H8', sessionMaxDurationSeconds: 0, levelWidth: 600, seed: 207, marioLives: 3, marioLarge: false, levelDifficulty: 3},
-	'i': {levelName: 'I9', sessionMaxDurationSeconds: 0, levelWidth: 600, seed: 208, marioLives: 3, marioLarge: false, levelDifficulty: 4},
-	'j': {levelName: 'J10', sessionMaxDurationSeconds: 0, levelWidth: 600, seed: 209, marioLives: 3, marioLarge: false, levelDifficulty: 5},
-	'k': {levelName: 'K11', sessionMaxDurationSeconds: 0, levelWidth: 600, seed: 210, marioLives: 3, marioLarge: false, levelDifficulty: 7},
-	'l': {levelName: 'L12', sessionMaxDurationSeconds: 0, levelWidth: 600, seed: 211, marioLives: 3, marioLarge: false, levelDifficulty: 8},
-	'm': {levelName: 'M13', sessionMaxDurationSeconds: 0, levelWidth: 600, seed: 212, marioLives: 3, marioLarge: false, levelDifficulty: 9},
-	'n': {levelName: 'N14', sessionMaxDurationSeconds: 0, levelWidth: 700, seed: 213, marioLives: 3, marioLarge: false, levelDifficulty: 10},
-	'o': {levelName: 'O15', sessionMaxDurationSeconds: 0, levelWidth: 700, seed: 214, marioLives: 3, marioLarge: false, levelDifficulty: 11},
-	'p': {levelName: 'P16', sessionMaxDurationSeconds: 0, levelWidth: 800, seed: 215, marioLives: 5, marioLarge: true, levelDifficulty: 12},
-	'q': {levelName: 'Q17', sessionMaxDurationSeconds: 0, levelWidth: 800, seed: 216, marioLives: 5, marioLarge: true, levelDifficulty: 13},
-	'r': {levelName: 'R18', sessionMaxDurationSeconds: 0, levelWidth: 800, seed: 217, marioLives: 5, marioLarge: true, levelDifficulty: 14},
-	's': {levelName: 'S19', sessionMaxDurationSeconds: 0, levelWidth: 800, seed: 218, marioLives: 5, marioLarge: true, levelDifficulty: 15, JumpLengthVariation: 3},
-	't': {levelName: 'T20', sessionMaxDurationSeconds: 0, levelWidth: 1000, seed: 219, marioLives: 5, marioLarge: true, levelDifficulty: 15, JumpMinLength: 3, JumpLengthVariation: 3}
+	'a': {levelName: 'A1', sessionMaxDurationSeconds: 0, levelWidth: 1000, seed: 200, marioLives: 3, marioLarge: true, levelDifficulty: 0, levelType: 0, levelFlowerAllowed: false, ValueMinOddsTubes: 0, ValueMultiOddsTubes: 0, ValueMinOddsJump: 0, ValueMultiOddsJump: 0, CoinsMinLineStartOffset: 10, CoinsMinLineEndOffset: 10, BlocksMinLineStartOffset: 10, BlocksMinLineEndOffset: 10, ValueOddsStraight: 10, ValueOddsHillStraight: 0, EnemyCreationCeilControl: 400},
+	'b': {levelName: 'B2', sessionMaxDurationSeconds: 0, levelWidth: 800, seed: 201, marioLives: 3, marioLarge: true, levelDifficulty: 0, levelType: 0, levelFlowerAllowed: false, ValueMinOddsTubes: 0, ValueMultiOddsTubes: 0, ValueMinOddsJump: 0, ValueMultiOddsJump: 0, CoinsMinLineStartOffset: 7, CoinsMinLineEndOffset: 7, BlocksMinLineStartOffset: 7, BlocksMinLineEndOffset: 7, ValueOddsStraight: 10, ValueOddsHillStraight: 0, EnemyCreationCeilControl: 150},
+	'c': {levelName: 'C3', sessionMaxDurationSeconds: 0, levelWidth: 800, seed: 202, marioLives: 3, marioLarge: true, levelDifficulty: 0, levelType: 0, levelFlowerAllowed: false, ValueMinOddsTubes: 0, ValueMultiOddsTubes: 0, ValueMinOddsJump: 0, ValueMultiOddsJump: 0, CoinsMinLineStartOffset: 3, CoinsMinLineEndOffset: 3, BlocksMinLineStartOffset: 5, BlocksMinLineEndOffset: 5, ValueOddsStraight: 10, ValueOddsHillStraight: 3, EnemyCreationCeilControl: 125},
+	'd': {levelName: 'D4', sessionMaxDurationSeconds: 0, levelWidth: 800, seed: 203, marioLives: 3, marioLarge: false, levelDifficulty: 1, levelType: 0, levelFlowerAllowed: false, ValueMinOddsTubes: 0, ValueMultiOddsTubes: 0, ValueMinOddsJump: 1, ValueMultiOddsJump: 1, CoinsMinLineStartOffset: 1, CoinsMinLineEndOffset: 1, BlocksMinLineStartOffset: 2, BlocksMinLineEndOffset: 2, ValueOddsStraight: 20, ValueOddsHillStraight: 10, EnemyCreationCeilControl: 100},
+	'e': {levelName: 'E5', sessionMaxDurationSeconds: 0, levelWidth: 800, seed: 204, marioLives: 3, marioLarge: false, levelDifficulty: 1, levelType: 0},
+	'f': {levelName: 'F6', sessionMaxDurationSeconds: 0, levelWidth: 700, seed: 205, marioLives: 3, marioLarge: false, levelDifficulty: 2, levelType: 0},
+	'g': {levelName: 'G7', sessionMaxDurationSeconds: 0, levelWidth: 700, seed: 206, marioLives: 3, marioLarge: false, levelDifficulty: 2, levelType: 0},
+	'h': {levelName: 'H8', sessionMaxDurationSeconds: 0, levelWidth: 600, seed: 207, marioLives: 3, marioLarge: false, levelDifficulty: 3, levelType: 0},
+	'i': {levelName: 'I9', sessionMaxDurationSeconds: 0, levelWidth: 600, seed: 208, marioLives: 3, marioLarge: false, levelDifficulty: 4, levelType: 0},
+	'j': {levelName: 'J10', sessionMaxDurationSeconds: 0, levelWidth: 600, seed: 209, marioLives: 3, marioLarge: false, levelDifficulty: 5, levelType: 0},
+	'k': {levelName: 'K11', sessionMaxDurationSeconds: 0, levelWidth: 600, seed: 210, marioLives: 3, marioLarge: false, levelDifficulty: 7, levelType: 0},
+	'l': {levelName: 'L12', sessionMaxDurationSeconds: 0, levelWidth: 600, seed: 211, marioLives: 3, marioLarge: false, levelDifficulty: 8, levelType: 0},
+	'm': {levelName: 'M13', sessionMaxDurationSeconds: 0, levelWidth: 600, seed: 212, marioLives: 3, marioLarge: false, levelDifficulty: 9, levelType: 0},
+	'n': {levelName: 'N14', sessionMaxDurationSeconds: 0, levelWidth: 700, seed: 213, marioLives: 3, marioLarge: false, levelDifficulty: 10, levelType: 0},
+	'o': {levelName: 'O15', sessionMaxDurationSeconds: 0, levelWidth: 700, seed: 214, marioLives: 3, marioLarge: false, levelDifficulty: 11, levelType: 0},
+	'p': {levelName: 'P16', sessionMaxDurationSeconds: 0, levelWidth: 800, seed: 215, marioLives: 5, marioLarge: true, levelDifficulty: 12, levelType: 0},
+	'q': {levelName: 'Q17', sessionMaxDurationSeconds: 0, levelWidth: 800, seed: 216, marioLives: 5, marioLarge: true, levelDifficulty: 13, levelType: 0},
+	'r': {levelName: 'R18', sessionMaxDurationSeconds: 0, levelWidth: 800, seed: 217, marioLives: 5, marioLarge: true, levelDifficulty: 14, levelType: 0},
+	's': {levelName: 'S19', sessionMaxDurationSeconds: 0, levelWidth: 800, seed: 218, marioLives: 5, marioLarge: true, levelDifficulty: 15, levelType: 0, JumpLengthVariation: 3},
+	't': {levelName: 'T20', sessionMaxDurationSeconds: 0, levelWidth: 1000, seed: 219, marioLives: 5, marioLarge: true, levelDifficulty: 15, levelType: 0, JumpMinLength: 3, JumpLengthVariation: 3}
 };
 
 var Experiment = {
