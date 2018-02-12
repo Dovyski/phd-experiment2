199ff729206358d41f8de73ec5cf0af731f78887
Fernando Bevilacqua <dovyski@gmail.com>
2018-02-12 09:35:51 +0100

    Add COTS Mario games into database

--- backend/init.php
+++ backend/init.php
@@ -15,11 +15,16 @@ $aDb->query('CREATE TABLE games (id PRIMARY KEY, name VARCHAR(100))');
 $aDb->query('CREATE INDEX idx_logs_fk_game ON logs (fk_game)');
 $aDb->query('CREATE INDEX idx_questionnaire_fk_game ON questionnaires (fk_game)');
 
-// TODO: move this to config
 $aDb->query('INSERT INTO games (id, name) VALUES (1, \'Mushroom\')');
 $aDb->query('INSERT INTO games (id, name) VALUES (2, \'Tetris\')');
 $aDb->query('INSERT INTO games (id, name) VALUES (3, \'Platformer\')');
-$aDb->query('INSERT INTO games (id, name) VALUES (4, \'COTS\')');
+$aDb->query('INSERT INTO games (id, name) VALUES (4, \'Mario-A1\')');
+$aDb->query('INSERT INTO games (id, name) VALUES (5, \'Mario-A2\')');
+$aDb->query('INSERT INTO games (id, name) VALUES (6, \'Mario-A3\')');
+$aDb->query('INSERT INTO games (id, name) VALUES (7, \'Mario-B1\')');
+$aDb->query('INSERT INTO games (id, name) VALUES (8, \'Mario-B2\')');
+$aDb->query('INSERT INTO games (id, name) VALUES (9, \'Mario-B3\')');
+$aDb->query('INSERT INTO games (id, name) VALUES (10, \'Mario-C1\')');
 
 echo 'Ok, database initialized';
 
